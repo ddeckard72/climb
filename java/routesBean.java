911f9863564be2a05c7fcd82b52927bb780d87b1
Don Deckard <don_deckard@yahoo.com>
2016-10-05 22:59:03 -0400

    Added start of route javabean

---
+++ java/routesBean.java
@@ -0,0 +1,40 @@
+public class RoutesBean implements java.io.Serializable
+{
+   private String location = null;
+   private String country = null;
+   private String city = null;
+   private String state = null;
+   private String region = null;
+   private String description = null;
+   private String usRating = null;
+   private String length = null;
+   private String firstAccent = null;
+   private String route = null;
+   private String routeType = null;
+   private String section = null;
+   private int numeric = 0;
+   private int gpsLong = 0;
+   private int gpsLat = 0;
+   private String dateAdded = null;
+
+   public RoutesBean() {
+   }
+   public String getLocation(){
+      return location;
+   }
+   public String getCountry(){
+      return country;
+   }
+   public int getCity(){
+      return city;
+   }
+   public void setLocation(String location){
+      this.location = location;
+   }
+   public void setCountry(String country){
+      this.country = country;
+   }
+   public void setCity(String city){
+      this.city = city;
+   }
+}
